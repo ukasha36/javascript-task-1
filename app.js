46391c7e11bbd613681e384955ab8e2f224126b5
ukasha36 <ukashashabbir36@gmail.com>
2023-08-31 12:59:30 +0500

    Sayalni Js Task

---
+++ app.js
@@ -0,0 +1,46 @@
+// Q1 
+// let greet = "Hello";
+// alert(greet);
+
+// Q2
+// let err="Error ! Please enter a valid password."
+// alert(err);
+
+
+//Q3
+// let js="Welcone to Js Land.. \n Happy Coding.."
+// alert(js);
+
+
+
+//Q4
+// let message="Welcone to Js Land.."
+// alert(message);
+// let message="Happy Coding.."
+// alert(message);
+
+//Q3
+// let js="Welcone to Js Land.. \n Happy Coding.."
+// alert(js);
+
+
+// Assigment -- 2
+
+let username;
+let Myname="Muhammad Ukasha";
+let message = "Hello World "
+alert(message);
+let studentname="Muhammad Ukasha";
+let studentage = "15 years old";
+let studentstatus= "certified Mobile Application Development";
+alert(studentname);
+alert(studentage);
+alert(studentstatus);
+let pizza = "pizza \npizz \npiz \npi \np"
+alert(pizza);
+let email =  "ukashashabbir@gmail.com "
+alert("My Email is : " + email);
+let book = "A samter way to learn javascript"
+alert(book);
+document.write("Yeah ! I can write HTML content through javascript");
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
